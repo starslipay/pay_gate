5a7a11c865cac443036423a4b131bdbecb5f3453
starsli <starsli@tencent.com>
2026-07-22 16:04:08 +0800

    save

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -6,7 +6,7 @@ IMAGE_NAME="${MODULE_NAME}:${VERSION}"
 docker rm -f $MODULE_NAME
 docker rmi -f $IMAGE_NAME
 docker build -t $IMAGE_NAME .
-docker run -d --name $MODULE_NAME --network dev_pay_net -p 30888:8080 $IMAGE_NAME
+docker run -d --name $MODULE_NAME --network dev_pay_net -p 30888:8888 $IMAGE_NAME
 # docker run -d --name pay_gate --network dev_pay_net -p 30888:8080 pay_gate:v1.0.0
 docker ps
 docker logs $MODULE_NAME
